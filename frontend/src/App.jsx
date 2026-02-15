7004d60ae5786506d431743da5d708cb724b5afa
Carlos-06021979 <carlosenguigarcia79@gmal.com>
2026-02-15 22:15:05 +0100

    Inicializo proyecto Frontend con React, Vite y Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="min-h-screen p-4">
+      <h1 className="text-3xl font-bold underline text-center text-blue-600">
+        Proyecto React AEMET
+      </h1>
+      <p className="text-center mt-4">
+        Si ves esto azul y centrado, Tailwind está funcionando.
+      </p>
+    </div>
+  );
+}
+
+export default App;
